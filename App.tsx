a29be1eab8be36236fcc53624ae4f8065b427ba8
Felipe Souza do Carmo <felipesouzadocarmo@MacBook-Air-de-Felipe.local>
2023-02-22 11:44:19 -0300

    primeiro desafio

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
+import { useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+  const [fontsLoaded] = useFonts({
+    'Overpass-Black': require('./src/assets/fonts/Overpass-Black.ttf'),
+    'Overpass-Bold': require('./src/assets/fonts/Overpass-Bold.ttf'),
+    'Overpass-ExtraBold': require('./src/assets/fonts/Overpass-ExtraBold.ttf'),
+    'Overpass-ExtraLight': require('./src/assets/fonts/Overpass-ExtraLight.ttf'),
+    'Overpass-Italic': require('./src/assets/fonts/Overpass-Italic.ttf'),
+    'Overpass-Light': require('./src/assets/fonts/Overpass-Light.ttf'),
+    'Overpass-Medium': require('./src/assets/fonts/Overpass-Medium.ttf'),
+    'Overpass-Regular': require('./src/assets/fonts/Overpass-Regular.ttf'),
+    'Overpass-SemiBold': require('./src/assets/fonts/Overpass-SemiBold.ttf'),
+    'Overpass-Thin': require('./src/assets/fonts/Overpass-Thin.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  return(
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontFamily: 'Overpass-Black' }}>7DaysOfCode de React Native com Expo</Text>
+      <Text style={{ fontFamily: 'Overpass-Black',fontSize:20 }}>Ilda Neta</Text>
       <StatusBar style="auto" />
     </View>
   );
